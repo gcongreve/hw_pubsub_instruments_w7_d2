dcc68123848e88f41737452a525bc1e97a493084
Graeme Congreve <junkmailgmc@gmail.com>
2018-11-27 18:01:48 +0000

    added publish instrument index

--- src/views/select_instrument_view.js
+++ src/views/select_instrument_view.js
@@ -5,15 +5,24 @@ const SelectInstumentView = function () {
 
 };
 
-const dropDownView = document.querySelector('select#instrument-families')
+
 
 
 SelectInstumentView.prototype.bindEvents = function () {
-  PubSub.subscribe("InstrumentFamilies:all-instruments", (evt) => {
-    const allInstruments = evt.detail;
-    console.log("all instros:", allInstruments)
+  const dropDownView = document.querySelector('select#instrument-families')
+
+  PubSub.subscribe("InstrumentFamilies:all-instruments", (instrumentData) => {
+    const allInstruments = instrumentData.detail;
     this.createDropDown(allInstruments)
-  })
+  });
+
+   dropDownView.addEventListener('change', (event) => {
+    const instrumentIndex = event.target.value;
+    PubSub.publish('SelectInstumentView:instrument-index', instrumentIndex);
+    console.log(instrumentIndex);
+  });
+
+
 };
 
 
