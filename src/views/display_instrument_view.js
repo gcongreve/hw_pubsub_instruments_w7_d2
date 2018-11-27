e4e0259e2fce9074abd184fc11f643dc9e65725b
Graeme Congreve <junkmailgmc@gmail.com>
2018-11-27 19:55:14 +0000

    can display chosen instrument on screen

--- src/views/display_instrument_view.js
+++ src/views/display_instrument_view.js
@@ -9,7 +9,15 @@ DisplayInstumentView.prototype.bindEvents = function () {
   PubSub.subscribe("InstrumentFamilies:selected-instrument", (evt) => {
     const instrument = evt.detail;
     console.log("display:", instrument);
+    this.displayInstument(instrument);
   })
+};
+
+DisplayInstumentView.prototype.displayInstument = function (instrument) {
+  const instrumentWindow = document.querySelector('#instrument-display');
+  const instrumentName = document.createElement('h2');
+  instrumentName.textContent = instrument.name;
+  instrumentWindow.appendChild(instrumentName)
 };
 
 module.exports = DisplayInstumentView;
